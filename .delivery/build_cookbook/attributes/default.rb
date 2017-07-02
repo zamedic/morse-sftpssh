b49e7f61d483f5c10c9b11d9d9434843e43cd91a
Marc Arndt <marc@marcarndt.com>
2017-07-02 19:31:08 +0200

    adding codacith checks

---
+++ .delivery/build_cookbook/attributes/default.rb
@@ -0,0 +1,6 @@
+default['coffee-truck']['release']['email'] = 'marc@marcarndt.com'
+default['coffee-truck']['release']['user'] = 'Marc Arndt'
+default['delivery']['config']['truck']['update_dependencies']['include'] = 'com.marcarndt.morse:*'
+default['delivery']['config']['truck']['update_dependencies']['active'] = true
+default['delivery']['config']['truck']['codacy']['upload'] = true
+default['delivery']['config']['truck']['codacy']['token'] = 'ada28a8b2d3c49e7a610ef8251d10579'
